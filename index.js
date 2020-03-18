2f45b398f6b81ca2c01efbecc8fc7f0646b330ae
ruzina75@gmail.com <ruzina75@gmail.com>
2020-03-18 20:37:22 +0000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Rock, Paper, Scissors");
+console.log("Welcome to Rock, Paper, Scissors\n");
+
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const options = ["rock", "paper", "scissors"];
+
+function handleUserResponse (userResponse) {
+    const userChoice = options.indexOf(userResponse.toLowerCase());
+    
+    if (userChoice < 0) {
+        return readline.question("Please choose either Rock, Paper, Scissors\n", handleUserResponse);
+    }
+
+    const computerChoice = Math.floor(Math.random() * 3);
+    
+    if (userChoice === computerChoice) {
+        console.log("You drew");
+      } else if (userChoice === 0 && computerChoice === 2 || userChoice === 1 && computerChoice === 0 || userChoice === 2 && computerChoice === 1) {
+        console.log("You won");
+      } else {
+        console.log("You lost");
+      }
+
+    readline.close();
+}
+
+readline.question("Please choose either Rock, Paper, Scissors\n", handleUserResponse);
